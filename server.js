d8038adc05591a6803249d0499b32eefd8331754
reetkaur1835 <144295525+reetkaur1835@users.noreply.github.com>
2023-10-15 06:06:00 -0400

    commit - 2

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req,res)=>{
     res.send('Hello World')
 })
 
+app.get('/blog', (req,res)=>{
+    res.send('Hello blog')
+})
+
 app.listen(3000, ()=>{
     console.log("Running on port 3000")
 })
