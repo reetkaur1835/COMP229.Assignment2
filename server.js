179db32a92038e9f864aac2baa4f859a6efa620c
reetkaur1835 <144295525+reetkaur1835@users.noreply.github.com>
2023-10-15 08:59:04 -0400

    get all products

--- server.js
+++ server.js
@@ -22,7 +22,17 @@ app.get('/blog', (req,res)=>{
     res.send('Hello blog')
 })
 
-app.post('/product', async(req,res)=>{
+app.get('/products', async(req,res)=>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+        
+    } catch (error) {
+        res.status(500).json({message : error.message})
+    }
+})
+
+app.post('/products', async(req,res)=>{
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product)
