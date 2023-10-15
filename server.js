034ad1d8547a115d0a088483afdadf54564878d0
reetkaur1835 <144295525+reetkaur1835@users.noreply.github.com>
2023-10-15 05:48:33 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+//routes
+app.get('/', (req,res)=>{
+    res.send('Hello World')
+})
+
+app.listen(3000, ()=>{
+    console.log("Running on port 3000")
+})
+
